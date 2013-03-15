d8aa77f9d6e123a9bdd8505721511caa71fbbf03
cmb69 <cmbecker69@gmx.de>
2013-03-15 14:56:33 +0000

    extended copyright to 2013

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@
 /**
  * Back-end of Boilerplate_XH.
  *
- * Copyright (c) 2012 Christoph M. Becker (see license.txt)
+ * Copyright (c) 2012-2013 Christoph M. Becker (see license.txt)
  */
 
 
@@ -24,7 +24,7 @@ function boilerplate_version() {
     return '<h1><a href="http://3-magi.net/?CMSimple_XH/Boilerplate_XH">Boilerplate_XH</a></h1>'."\n"
 	    .tag('img class="boilerplate_plugin_icon" src="'.$pth['folder']['plugins'].'boilerplate/boilerplate.png" alt="Plugin icon"')."\n"
 	    .'<p style="margin-top: 1em">Version: '.BOILERPLATE_VERSION.'</p>'."\n"
-	    .'<p>Copyright &copy; 2012 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'."\n"
+	    .'<p>Copyright &copy; 2012-2013 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'."\n"
 	    .'<p class="boilerplate_license">This program is free software: you can redistribute it and/or modify'
 	    .' it under the terms of the GNU General Public License as published by'
 	    .' the Free Software Foundation, either version 3 of the License, or'
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /**
  * Front-end of Boilerplate_XH.
  *
- * Copyright (c) 2012 Christoph M. Becker (see license.txt)
+ * Copyright (c) 2012-2013 Christoph M. Becker (see license.txt)
  */
 
 
