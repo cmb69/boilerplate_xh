b8cc8654cff0b39acbe882fa5cac096b942960ee
cmb69 <cmbecker69@gmx.de>
2013-03-15 22:26:59 +0000

    refactored about and system check as info view

--- admin.php
+++ admin.php
@@ -21,72 +21,48 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 
 
 /**
- * Returns the version information view.
+ * Returns the plugin information view.
  *
- * @global array  The paths of system files and folders.
  * @return string  The (X)HTML.
  */
-function Boilerplate_version()
-{
-    global $pth;
-
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Boilerplate_XH">Boilerplate_XH</a></h1>'
-	. tag('img src="' . $pth['folder']['plugins'] . 'boilerplate/boilerplate.png" alt="Plugin icon" style="float: left; margin: 0 16px 0 0"')
-	. '<p style="margin-top: 1em">Version: ' . BOILERPLATE_VERSION . '</p>'
-	. '<p>Copyright &copy; 2012-2013 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'
-	. '<p style="text-align: justify">This program is free software: you can redistribute it and/or modify'
-	. ' it under the terms of the GNU General Public License as published by'
-	. ' the Free Software Foundation, either version 3 of the License, or'
-	. ' (at your option) any later version.</p>'
-	. '<p style="text-align: justify">This program is distributed in the hope that it will be useful,'
-	. ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	. ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	. ' GNU General Public License for more details.</p>'
-	. '<p style="text-align: justify">You should have received a copy of the GNU General Public License'
-	. ' along with this program.  If not, see'
-	. ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>';
-}
-
-
-/**
- * Returns the requirements information view.
- *
- * @global array  The paths of system files and folders.
- * @global array  The localization of the core.
- * @global array  The localization of the plugins.
- * @return string  The (X)HTML.
- */
-function boilerplate_systemCheck() // RELEASE-TODO
+function Boilerplate_info() // RELEASE-TODO
 {
     global $pth, $tx, $plugin_tx;
 
-    $phpVersion = '4.3.0';
     $ptx = $plugin_tx['boilerplate'];
-    $imgdir = $pth['folder']['plugins'] . 'boilerplate/images/';
-    $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
-    $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
-    $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
-    $o = '<h4>' . $ptx['syscheck_title'] . '</h4>'
-	. (version_compare(PHP_VERSION, $phpVersion) >= 0 ? $ok : $fail)
-	. '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], $phpVersion)
-	. tag('br');
-    foreach (array('pcre') as $ext) {
-	$o .= (extension_loaded($ext) ? $ok : $fail)
-	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext) . tag('br');
+    $labels = array(
+	'syscheck' => $ptx['syscheck_title'],
+	'about' => $ptx['about']
+    );
+    $labels = array_map('Boilerplate_hsc', $labels);
+    $phpVersion = '4.3.0';
+    foreach (array('ok', 'warn', 'fail') as $state) {
+        $images[$state] = $pth['folder']['plugins']
+	    . "boilerplate/images/$state.png";
     }
-    $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	. '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br') . tag('br');
-    $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	. '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br') . tag('br');
-    foreach (array('config/', 'css/', 'languages/') as $folder) {
+    $checks = array();
+    $checks[sprintf($ptx['syscheck_phpversion'], $phpVersion)] =
+        version_compare(PHP_VERSION, $phpVersion) >= 0 ? 'ok' : 'fail';
+    foreach (array('pcre') as $ext) {
+	$checks[sprintf($ptx['syscheck_extension'], $ext)] =
+	    extension_loaded($ext) ? 'ok' : 'fail';
+    }
+    $checks[$ptx['syscheck_magic_quotes']] =
+        !get_magic_quotes_runtime() ? 'ok' : 'fail';
+    $checks[$ptx['syscheck_encoding']] =
+        strtoupper($tx['meta']['codepage']) == 'UTF-8' ? 'ok' : 'warn';
+    foreach (array('config/', 'css', 'languages/') as $folder) {
 	$folders[] = $pth['folder']['plugins'] . 'boilerplate/' . $folder;
     }
     $folders[] = Boilerplate_dataFolder();
     foreach ($folders as $folder) {
-	$o .= (is_writable($folder) ? $ok : $warn)
-	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder) . tag('br');
+	$checks[sprintf($ptx['syscheck_writable'], $folder)] =
+            is_writable($folder) ? 'ok' : 'warn';
     }
-    return $o;
+    $icon = $pth['folder']['plugins'] . 'boilerplate/boilerplate.png';
+    $version = BOILERPLATE_VERSION;
+    $bag = compact('labels', 'images', 'checks', 'icon', 'version');
+    return Boilerplate_render('info', $bag);
 }
 
 
@@ -284,7 +260,7 @@ if (isset($boilerplate) && $boilerplate == 'true') {
     $o .= print_plugin_admin('on');
     switch ($admin) {
     case '':
-	$o .= Boilerplate_version() . tag('hr') . Boilerplate_systemCheck();
+	$o .= Boilerplate_info();
 	break;
     case 'plugin_main':
 	switch ($action) {
--- languages/en.php
+++ languages/en.php
@@ -13,6 +13,8 @@ $plugin_tx['boilerplate']['syscheck_encoding']="Encoding 'UTF-8' configured";
 $plugin_tx['boilerplate']['syscheck_magic_quotes']="magic_quotes_runtime off";
 $plugin_tx['boilerplate']['syscheck_writable']="Folder '%s' writable";
 
+$plugin_tx['boilerplate']['about']="About Boilerplate_XH";
+
 $plugin_tx['boilerplate']['cf_folder_data']="Path of a folder relative to the CMSimple root directory, where to store the plugin's data, e.g. \"userfiles/boilerplate/\". Leave empty to store in the plugin's data/ folder.";
 
 ?>
