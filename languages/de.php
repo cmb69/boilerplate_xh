f18be04742cceb31534ba5984258a1446a31814b
cmb69 <cmbecker69@gmx.de>
2013-03-15 14:53:54 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,18 @@
+<?php
+
+$plugin_tx['boilerplate']['menu_main']="Textbausteine";
+
+$plugin_tx['boilerplate']['label_create']="Anlegen";
+
+$plugin_tx['boilerplate']['error_invalid_name']="Ungültiger Textbaustein-Name (darf nur aus a-z, A-Z, 0-9, _ and - bestehen):";
+
+$plugin_tx['boilerplate']['syscheck_title']="Systemprüfung";
+$plugin_tx['boilerplate']['syscheck_phpversion']="PHP-Version &ge; %s";
+$plugin_tx['boilerplate']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['boilerplate']['syscheck_encoding']="UTF-8-Kodierung eingestellt";
+$plugin_tx['boilerplate']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['boilerplate']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+$plugin_tx['boilerplate']['cf_folder_data']="Pfad eines Ordners relativ zum CMSimple Installationsverzeichnis, wo die Plugindaten gespeichert werden sollen, z.B. \"userfiles/boilerplate/\". Leer lassen, um in den data/ Ordner im Pluginverzeichnis zu speichern.";
+
+?>
--- languages/en.php
+++ languages/en.php
@@ -1,22 +1,18 @@
 <?php
 
 $plugin_tx['boilerplate']['menu_main']="Text Blocks";
-$plugin_tx['boilerplate']['menu_css']="Stylesheet";
-$plugin_tx['boilerplate']['menu_config']="Config";
-$plugin_tx['boilerplate']['menu_language']="Language";
-$plugin_tx['boilerplate']['menu_help']="Help";
 
 $plugin_tx['boilerplate']['label_create']="Create";
 
-$plugin_tx['boilerplate']['error_invalid_name']="Invalid boilerplate name (must only consist of a-z, A-Z, 0-9, _ and -):";
+$plugin_tx['boilerplate']['error_invalid_name']="Invalid text block name (must only consist of a-z, A-Z, 0-9, _ and -):";
 
 $plugin_tx['boilerplate']['syscheck_title']="System check";
 $plugin_tx['boilerplate']['syscheck_phpversion']="PHP version &ge; %s";
 $plugin_tx['boilerplate']['syscheck_extension']="Extension '%s' loaded";
 $plugin_tx['boilerplate']['syscheck_encoding']="Encoding 'UTF-8' configured";
-$plugin_tx['boilerplate']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['boilerplate']['syscheck_magic_quotes']="magic_quotes_runtime off";
 $plugin_tx['boilerplate']['syscheck_writable']="Folder '%s' writable";
 
-$plugin_tx['boilerplate']['cf_folder_data']="Path to a folder relative to the CMSimple root directory, where to store the plugin's data. E.g. \"userfiles/boilerplate/\". Leave empty to store in the plugin's data/ folder.";
+$plugin_tx['boilerplate']['cf_folder_data']="Path of a folder relative to the CMSimple root directory, where to store the plugin's data, e.g. \"userfiles/boilerplate/\". Leave empty to store in the plugin's data/ folder.";
 
 ?>
