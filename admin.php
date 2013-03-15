be65b1a3ff63ffc38e3abcfe56148d61be09cb98
cmb69 <cmbecker69@gmx.de>
2013-03-15 17:32:56 +0000

    reformatted code according to PEAR coding standards

--- admin.php
+++ admin.php
@@ -3,10 +3,17 @@
 /**
  * Back-end of Boilerplate_XH.
  *
- * Copyright (c) 2012-2013 Christoph M. Becker (see license.txt)
+ * @package	Boilerplate
+ * @copyright	Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
+ * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version     $Id$
+ * @link	http://3-magi.net/?CMSimple_XH/Boilerplate_XH
  */
 
 
+/*
+ * Prevent direct access.
+ */
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
@@ -16,62 +23,68 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 /**
  * Returns the version information view.
  *
+ * @global array  The paths of system files and folders.
  * @return string  The (X)HTML.
  */
-function boilerplate_version() {
+function Boilerplate_version()
+{
     global $pth;
 
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Boilerplate_XH">Boilerplate_XH</a></h1>'."\n"
-	    .tag('img class="boilerplate_plugin_icon" src="'.$pth['folder']['plugins'].'boilerplate/boilerplate.png" alt="Plugin icon"')."\n"
-	    .'<p style="margin-top: 1em">Version: '.BOILERPLATE_VERSION.'</p>'."\n"
-	    .'<p>Copyright &copy; 2012-2013 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'."\n"
-	    .'<p class="boilerplate_license">This program is free software: you can redistribute it and/or modify'
-	    .' it under the terms of the GNU General Public License as published by'
-	    .' the Free Software Foundation, either version 3 of the License, or'
-	    .' (at your option) any later version.</p>'."\n"
-	    .'<p class="boilerplate_license">This program is distributed in the hope that it will be useful,'
-	    .' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	    .' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	    .' GNU General Public License for more details.</p>'."\n"
-	    .'<p class="boilerplate_license">You should have received a copy of the GNU General Public License'
-	    .' along with this program.  If not, see'
-	    .' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>'."\n";
+    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Boilerplate_XH">Boilerplate_XH</a></h1>'
+	. tag('img class="boilerplate_plugin_icon" src="' . $pth['folder']['plugins'] . 'boilerplate/boilerplate.png" alt="Plugin icon"')
+	. '<p style="margin-top: 1em">Version: ' . BOILERPLATE_VERSION . '</p>'
+	. '<p>Copyright &copy; 2012-2013 <a href="http://3-magi.net/">Christoph M. Becker</a></p>'
+	. '<p class="boilerplate_license">This program is free software: you can redistribute it and/or modify'
+	. ' it under the terms of the GNU General Public License as published by'
+	. ' the Free Software Foundation, either version 3 of the License, or'
+	. ' (at your option) any later version.</p>'
+	. '<p class="boilerplate_license">This program is distributed in the hope that it will be useful,'
+	. ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
+	. ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
+	. ' GNU General Public License for more details.</p>'
+	. '<p class="boilerplate_license">You should have received a copy of the GNU General Public License'
+	. ' along with this program.  If not, see'
+	. ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>';
 }
 
 
 /**
  * Returns the requirements information view.
  *
+ * @global array  The paths of system files and folders.
+ * @global array  The localization of the core.
+ * @global array  The localization of the plugins.
  * @return string  The (X)HTML.
  */
-function boilerplate_system_check() { // RELEASE-TODO
+function boilerplate_systemCheck() // RELEASE-TODO
+{
     global $pth, $tx, $plugin_tx;
 
-    define('BOILERPLATE_PHP_VERSION', '4.3.0');
+    $phpVersion = '4.3.0';
     $ptx = $plugin_tx['boilerplate'];
-    $imgdir = $pth['folder']['plugins'].'boilerplate/images/';
-    $ok = tag('img src="'.$imgdir.'ok.png" alt="ok"');
-    $warn = tag('img src="'.$imgdir.'warn.png" alt="warning"');
-    $fail = tag('img src="'.$imgdir.'fail.png" alt="failure"');
-    $o = '<h4>'.$ptx['syscheck_title'].'</h4>'
-	    .(version_compare(PHP_VERSION, BOILERPLATE_PHP_VERSION) >= 0 ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.sprintf($ptx['syscheck_phpversion'], BOILERPLATE_PHP_VERSION)
-	    .tag('br')."\n";
+    $imgdir = $pth['folder']['plugins'] . 'boilerplate/images/';
+    $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
+    $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
+    $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
+    $o = '<h4>' . $ptx['syscheck_title'] . '</h4>'
+	. (version_compare(PHP_VERSION, $phpVersion) >= 0 ? $ok : $fail)
+	. '&nbsp;&nbsp;' . sprintf($ptx['syscheck_phpversion'], $phpVersion)
+	. tag('br');
     foreach (array('pcre') as $ext) {
 	$o .= (extension_loaded($ext) ? $ok : $fail)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_extension'], $ext).tag('br')."\n";
+	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext) . tag('br');
     }
     $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_magic_quotes'].tag('br').tag('br')."\n";
+	. '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br') . tag('br');
     $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_encoding'].tag('br').tag('br')."\n";
+	. '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br') . tag('br');
     foreach (array('config/', 'css/', 'languages/') as $folder) {
-	$folders[] = $pth['folder']['plugins'].'boilerplate/'.$folder;
+	$folders[] = $pth['folder']['plugins'] . 'boilerplate/' . $folder;
     }
-    $folders[] = boilerplate_data_folder();
+    $folders[] = Boilerplate_dataFolder();
     foreach ($folders as $folder) {
 	$o .= (is_writable($folder) ? $ok : $warn)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_writable'], $folder).tag('br')."\n";
+	    . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder) . tag('br');
     }
     return $o;
 }
@@ -83,44 +96,51 @@ function boilerplate_system_check() { // RELEASE-TODO
  * @param string $name
  * @return string  The (X)HTML.
  */
-function boilerplate_new($name) {
-    if (!boilerplate_valid_name($name)) {
-	return boilerplate_admin($name);
+function Boilerplate_new($name)
+{
+    if (!Boilerplate_validName($name)) {
+	return Boilerplate_admin($name);
     }
-    $fn = boilerplate_data_folder().$name.'.dat';
+    $fn = Boilerplate_filename($name);
     if (!file_exists($fn)) {
-	if (($fh = fopen($fn, 'x')) === FALSE) {
+	if (($fh = fopen($fn, 'x')) === false) {
 	    e('cntwriteto', 'file', $fn);
 	} else {
 	    fclose($fh);
 	}
     }
-    return boilerplate_edit($name);
+    return Boilerplate_edit($name);
 }
 
 
 /**
  * Returns the boilerplate edit view.
  *
+ * @global string  The site name.
+ * @global array  The paths of system files and folders.
+ * @global array  The configuration of the core.
+ * @global array  The localization of the core.
  * @param string $name
  * @return string  The (X)HTML.
  */
-function boilerplate_edit($name) {
-    global $tx, $sn, $cf, $pth;
+function Boilerplate_edit($name)
+{
+    global $sn, $pth, $cf, $tx;
 
-    $fn = boilerplate_data_folder().$name.'.dat';
-    if (($cnt = file_get_contents($fn)) === FALSE) {
+    $fn = Boilerplate_filename($name);
+    if (($content = file_get_contents($fn)) === false) {
 	e('cntopen', 'file', $fn);
-	return FALSE;
+	return false;
     }
-    $url = $sn.'?&amp;boilerplate&amp;admin=plugin_main&amp;action=save&amp;boilerplate_name='.urlencode($name);
-    $o = '<div class="plugineditcaption">Boilerplate: '.$name.'</div>'
-	    .'<form class="plugineditform" action="'.$url.'" method="POST">'
-	    .'<textarea class="plugintextarea" name="boilerplate_text" style="height: '.$cf['editor']['height'].'px">'
-		    .htmlspecialchars($cnt).'</textarea>'
-	    .(!function_exists('init_editor') || $cf['editor']['external'] == ''
-		    ? tag('input type="submit" class="submit" value="'.ucfirst($tx['action']['save']).'"') : '')
-	    .'</form>';
+    $action = $sn . '?&amp;boilerplate&amp;admin=plugin_main&amp;action=save&amp;boilerplate_name=' . urlencode($name);
+    $o = '<div class="plugineditcaption">Boilerplate: ' . $name . '</div>'
+	. '<form class="plugineditform" action="' . $action . '" method="post">'
+	. '<textarea class="plugintextarea" name="boilerplate_text" style="height: ' . $cf['editor']['height'] . 'px">'
+	. htmlspecialchars($content, ENT_NOQUOTES, 'UTF-8') . '</textarea>'
+	. (!function_exists('init_editor') || $cf['editor']['external'] == ''
+	    ? tag('input type="submit" class="submit" value="' . ucfirst($tx['action']['save']) . '"')
+	    : '')
+	. '</form>';
     init_editor(array('plugintextarea'));
     return $o;
 }
@@ -132,15 +152,18 @@ function boilerplate_edit($name) {
  * @param string $name
  * @return string  The (X)HTML.
  */
-function boilerplate_save($name) {
-    $fn = boilerplate_data_folder().$name.'.dat';
-    if (file_exists($fn) && !is_writable($fn)
-	    || ($fh = fopen($fn, 'w')) === FALSE
-	    || fwrite($fh, stsl($_POST['boilerplate_text'])) === FALSE) {
+function Boilerplate_save($name)
+{
+    $fn = Boilerplate_filename($name);
+    if (($fh = fopen($fn, 'w')) === false
+	|| fwrite($fh, stsl($_POST['boilerplate_text'])) === false)
+    {
 	e('cntsave', 'file', $fn);
     }
-    if (!empty($fh)) {fclose($fh);}
-    return boilerplate_admin();
+    if ($fh) {
+	fclose($fh);
+    }
+    return Boilerplate_admin();
 }
 
 
@@ -150,70 +173,85 @@ function boilerplate_save($name) {
  * @param string $name
  * @return string  The (X)HTML.
  */
-function boilerplate_delete($name) {
-    $fn = boilerplate_data_folder().$name.'.dat';
+function boilerplate_delete($name)
+{
+    $fn = Boilerplate_filename($name);
     if (!unlink($fn)) {
 	e('cntdelete', 'file', $fn);
     }
-    return boilerplate_admin();
+    return Boilerplate_admin();
 }
 
 
 /**
  * Returns the main administration view.
  *
+ * @global array  The paths of system files and folders.
+ * @global string  The script name.
+ * @global array  The localization of the core.
+ * @global array  The localization of the plugins.
  * @return string  The (X)HTML.
  */
-function boilerplate_admin() {
+function Boilerplate_admin()
+{
     global $pth, $sn, $tx, $plugin_tx;
 
     $ptx = $plugin_tx['boilerplate'];
-    $o = '<div id="boilerplate_admin" class="plugineditcaption">Boilerplate: '.$ptx['menu_main'].'</div>'."\n"
-	    .'<ul>';
+    $o = '<div id="boilerplate_admin" class="plugineditcaption">Boilerplate: '
+	. $ptx['menu_main'] . '</div>'
+	.'<ul>';
     $baseurl = $sn.'?&amp;boilerplate&amp;admin=plugin_main&amp;action=';
-    $o .= '<li><form action="'.$baseurl.'new" method="POST">'
-	    .tag('input type="text" name="boilerplate_name"')
-	    .tag('input type="submit" class="submit" value="'.$ptx['label_create'].'"')
-	    .'</form></li>';
+    $o .= '<li><form action="' . $baseurl . 'new" method="post">'
+	. tag('input type="text" name="boilerplate_name"')
+	. tag('input type="submit" class="submit" value="' . $ptx['label_create'] . '"')
+	. '</form></li>';
     $url = $baseurl.'edit&amp;boilerplate_name=';
-    foreach (glob(boilerplate_data_folder().'*.dat') as $file) {
+    foreach (glob(Boilerplate_filename('*')) as $file) {
 	$name = basename($file, '.dat');
 	$o .= '<li>'
-		.'<a href="'.$url.urlencode($name).'" title="'.ucfirst($tx['action']['edit']).'" style="float: left">'
-			.$name.'</a>'
-		.'<form action="'.$baseurl.'delete&amp;boilerplate_name='.urlencode($name).'"'
-			.' method="POST" style="float:left; margin-left: 1em">'
-		.tag('input type="image" src="'.$pth['folder']['plugins'].'boilerplate/images/delete.png"'
-			.' alt="'.ucfirst($tx['action']['delete']).'" title="'.ucfirst($tx['action']['delete']).'"')
-		.'</form>'
-		.tag('input type="text" readonly="readonly" value="{{{PLUGIN:boilerplate(\''.$name.'\');}}}" onclick="this.select()" style="float: left; margin-left: 1em"')
-		.'</li>'."\n";
+	    . '<a href="' . $url . urlencode($name) . '" title="' . ucfirst($tx['action']['edit']) . '" style="float: left">'
+	    . $name . '</a>'
+	    . '<form action="' . $baseurl . 'delete&amp;boilerplate_name=' . urlencode($name) . '"'
+	    . ' method="post" style="float:left; margin-left: 1em">'
+	    . tag('input type="image" src="' . $pth['folder']['plugins'] . 'boilerplate/images/delete.png"'
+	    . ' alt="' . ucfirst($tx['action']['delete']) . '" title="' . ucfirst($tx['action']['delete']) . '"')
+	    . '</form>'
+	    . tag('input type="text" readonly="readonly" value="{{{PLUGIN:boilerplate(\'' . $name . '\');}}}" onclick="this.select()" style="float: left; margin-left: 1em"')
+	    . '</li>';
     }
-    $o .= '</ul>'."\n";
+    $o .= '</ul>';
     return $o;
 }
 
 
-/**
+/*
  * Handle the plugin administration.
  */
-if (!empty($boilerplate)) {
+if (isset($boilerplate) && $boilerplate == 'true') {
     $o .= print_plugin_admin('on');
     switch ($admin) {
-	case '':
-	    $o .= boilerplate_version().tag('hr').boilerplate_system_check();
+    case '':
+	$o .= Boilerplate_version() . tag('hr') . Boilerplate_systemCheck();
+	break;
+    case 'plugin_main':
+	switch ($action) {
+	case 'new':
+	    $o .= Boilerplate_new(stsl($_POST['boilerplate_name']));
 	    break;
-	case 'plugin_main':
-	    switch ($action) {
-		case 'new': $o .= boilerplate_new(stsl($_POST['boilerplate_name'])); break;
-		case 'edit': $o .= boilerplate_edit(stsl($_GET['boilerplate_name'])); break;
-		case 'save': $o .= boilerplate_save(stsl($_GET['boilerplate_name'])); break;
-		case 'delete': $o .= boilerplate_delete(stsl($_GET['boilerplate_name'])); break;
-		default: $o .= boilerplate_admin();
-	    }
+	case 'edit':
+	    $o .= Boilerplate_edit(stsl($_GET['boilerplate_name']));
 	    break;
-	default:
-	    $o .= plugin_admin_common($action, $admin, $plugin);
+	case 'save':
+	    $o .= Boilerplate_save(stsl($_GET['boilerplate_name']));
+	    break;
+	case 'delete':
+	    $o .= Boilerplate_delete(stsl($_GET['boilerplate_name']));
+	    break;
+	default: $o .= Boilerplate_admin();
+	}
+	break;
+    default:
+	$o .= plugin_admin_common($action, $admin, $plugin);
     }
 }
 
--- index.php
+++ index.php
@@ -3,49 +3,64 @@
 /**
  * Front-end of Boilerplate_XH.
  *
- * Copyright (c) 2012-2013 Christoph M. Becker (see license.txt)
+ * @package	Boilerplate
+ * @copyright	Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
+ * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version     $Id$
+ * @link	http://3-magi.net/?CMSimple_XH/Boilerplate_XH
  */
 
 
+/*
+ * Prevent direct access.
+ */
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
 
+/**
+ * The version number of the plugin.
+ */
 define('BOILERPLATE_VERSION', '1beta1');
 
 
+/*
+ * For backward compatibility CMSimple_XH < 1.5
+ */
 if (!function_exists('evaluate_scripting')) {
-    function evaluate_scripting($txt) {
+    function evaluate_scripting($txt)
+    {
 	return $txt;
     }
 }
 
 
 /**
- * Returns the data folder.
+ * Returns the data folder path. Creates it, if necessary.
+ * Emits error messages on failure.
  *
+ * @global array  The paths of system files and folders.
+ * @global array  The configuration of the plugins.
  * @return string
  */
-function boilerplate_data_folder() {
+function Boilerplate_dataFolder()
+{
     global $pth, $plugin_cf;
 
     $pcf = $plugin_cf['boilerplate'];
     if ($pcf['folder_data'] == '') {
-	$fn = $pth['folder']['plugins'].'boilerplate/data/';
+	$fn = $pth['folder']['plugins'] . 'boilerplate/data/';
     } else {
-	$fn = $pth['folder']['base'].$pcf['folder_data'];
-    }
-    if (substr($fn, -1) != '/') {
-	$fn .= '/';
+	$fn = $pth['folder']['base'] . rtrim('/', $pcf['folder_data']) . '/';
     }
     if (file_exists($fn)) {
 	if (!is_dir($fn)) {
 	    e('cntopen', 'folder', $fn);
 	}
     } else {
-	if (!mkdir($fn, 0777, TRUE)) {
+	if (!mkdir($fn, 0777, true)) {
 	    e('cntwriteto', 'folder', $fn);
 	}
     }
@@ -54,40 +69,61 @@ function boilerplate_data_folder() {
 
 
 /**
+ * Returns the path of a boilerplate file.
+ *
+ * @param  string $name  The name of the text block.
+ * @return string
+ */
+function Boilerplate_filename($name)
+{
+    return Boilerplate_dataFolder() . $name . '.dat';
+}
+
+/**
  * Returns whether the given $name is valid.
  * Emits an error message, if the name is invalid.
  *
- * @param string $name
+ * @global string  Error messages to add to the (X)HTML.
+ * @global array  The localization of the plugins.
+ * @param  string $name
  * @return bool
  */
-function boilerplate_valid_name($name) {
+function Boilerplate_validName($name)
+{
     global $e, $plugin_tx;
 
     $ptx = $plugin_tx['boilerplate'];
-    if (!($valid = preg_match('/^[a-z0-9_\-]*$/su', $name))) {
-	$e .= '<li><b>'.$ptx['error_invalid_name'].'</b>'.tag('br').$name.'</li>'."\n";
+    $valid = preg_match('/^[a-z0-9_\-]*$/su', $name);
+    if (!$valid) {
+	$e .= '<li><b>' . $ptx['error_invalid_name'] . '</b>' . tag('br')
+	    . $name . '</li>' . PHP_EOL;
     }
     return $valid;
 }
 
 
 /**
- * Returns the text block.
+ * Returns a text block.
+ * On failure a error message is emitted and false is returned.
  *
  * @access public
- * @param string $name
+ *
+ * @param  string $name
  * @return string  The (X)HTML.
  */
-function boilerplate($name) {
-    if (!boilerplate_valid_name($name)) {
-	return FALSE;
+function Boilerplate($name)
+{
+    if (!Boilerplate_validName($name)) {
+	return false;
     }
-    $fn = boilerplate_data_folder().$name.'.dat';
-    if (is_readable($fn) && ($cnt = file_get_contents($fn)) !== FALSE) {
-	return evaluate_scripting($cnt);
+    $fn = Boilerplate_filename($name);
+    if (is_readable($fn)
+	&& ($content = file_get_contents($fn)) !== false)
+    {
+	return evaluate_scripting($content);
     } else {
 	e('cntopen', 'file', $fn);
-	return FALSE;
+	return false;
     }
 }
 
