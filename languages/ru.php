e86d0c62df2c2b33946c6e236c4eba9dbffb776b
cmb69 <cmbecker69@gmx.de>
2013-03-18 22:16:02 +0000

    added Russian and Slovak localization (contributed by Old resp. Tata)

---
+++ languages/ru.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Old aka Lybomyr Kydray
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+$plugin_tx['boilerplate']['menu_main']="Текстовые блоки";
+$plugin_tx['boilerplate']['menu_css']="Стили";
+$plugin_tx['boilerplate']['menu_config']="Конфигурация";
+$plugin_tx['boilerplate']['menu_language']="Язык";
+$plugin_tx['boilerplate']['menu_help']="Помощь";
+$plugin_tx['boilerplate']['label_create']="Создать";
+$plugin_tx['boilerplate']['error_invalid_name']="Неправильное имя шаблона (допустимо использование только букв <b>а-я</b>, <b>А-Я</b> и символов <b>_</b> и <b>-</b>).";
+$plugin_tx['boilerplate']['syscheck_title']="Проверка системы";
+$plugin_tx['boilerplate']['syscheck_phpversion']="Версия PHP &ge; %s";
+$plugin_tx['boilerplate']['syscheck_extension']="Расширение '%s' загружено";
+$plugin_tx['boilerplate']['syscheck_encoding']="Кодировка 'UTF-8' установлена";
+$plugin_tx['boilerplate']['syscheck_magic_quotes']="Экранирование спецсимволов (\"magic_quotes_runtime\") отключено";
+$plugin_tx['boilerplate']['syscheck_writable']="Папка '%s' доступна для записи";
+$plugin_tx['boilerplate']['cf_folder_data']="Путь к папке, где хранятся файлы данных, относительно корневого каталога CMSimple. Например, \"userfiles/boilerplate/\". Оставьте поле пустым для хранения данных в папке по умолчанию (каталог <b>data/</b> в директории плагина).";
+
+?>
---
+++ languages/sk.php
@@ -0,0 +1,18 @@
+<?php
+
+$plugin_tx['boilerplate']['menu_main']="Textové bloky";
+$plugin_tx['boilerplate']['menu_css']="Štýl";
+$plugin_tx['boilerplate']['menu_config']="Nastaviť";
+$plugin_tx['boilerplate']['menu_language']="Jazyk";
+$plugin_tx['boilerplate']['menu_help']="Pomocník";
+$plugin_tx['boilerplate']['label_create']="Vytvoriť";
+$plugin_tx['boilerplate']['error_invalid_name']="Nesprávny názov boilerplate (môže obsahovať iba znaky a-z, A-Z, 0-9, _ a -):";
+$plugin_tx['boilerplate']['syscheck_title']="Kontrola systému";
+$plugin_tx['boilerplate']['syscheck_phpversion']="PHP verzia &ge; %s";
+$plugin_tx['boilerplate']['syscheck_extension']="Rozšírenie '%s' načítané";
+$plugin_tx['boilerplate']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
+$plugin_tx['boilerplate']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['boilerplate']['syscheck_writable']="Adresár '%s' umožňuje zápis";
+$plugin_tx['boilerplate']['cf_folder_data']="Cesta k adresáru relatívna voči koreňovému adresáru CMSimple, kam sa uložia dáta pluginu. Napr. \"userfiles/boilerplate/\". Ak cestu neuvediete, dáta sa uložia do adresára  data/ pluginu.";
+
+?>
