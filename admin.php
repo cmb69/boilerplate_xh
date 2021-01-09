9d649b94cd88f769f3d9e418b4073ce553460a97
Christoph M. Becker <cmbecker69@gmx.de>
2021-01-09 16:54:38 +0100

    Extract class AdminController

--- admin.php
+++ admin.php
@@ -85,221 +85,6 @@ function Boilerplate_render($_template, $_bag)
     return $o;
 }
 
-/**
- * Returns the plugin information view.
- *
- * @return string (X)HTML.
- *
- * @global array             The paths of system files and folders.
- * @global array             The localization of the core.
- * @global array             The localization of the plugins.
- * @global Boilerplate\Model The model.
- */
-function Boilerplate_info() // RELEASE-TODO
-{
-    global $pth, $tx, $plugin_tx, $_Boilerplate;
-
-    $ptx = $plugin_tx['boilerplate'];
-    $labels = array(
-        'info' => $ptx['label_info'],
-        'syscheck' => $ptx['syscheck_title'],
-        'about' => $ptx['about'],
-        'logo' => $ptx['alt_logo']
-    );
-    $labels = array_map('XH_hsc', $labels);
-    $phpVersion = '5.4.0';
-    foreach (array('ok', 'warn', 'fail') as $state) {
-        $images[$state] = $pth['folder']['plugins']
-            . "boilerplate/images/$state.png";
-    }
-    $checks = array();
-    $checks[sprintf($ptx['syscheck_phpversion'], $phpVersion)]
-        = version_compare(PHP_VERSION, $phpVersion) >= 0 ? 'ok' : 'fail';
-    foreach (array('config/', 'css', 'languages/') as $folder) {
-        $folders[] = $pth['folder']['plugins'] . 'boilerplate/' . $folder;
-    }
-    $folders[] = $_Boilerplate->getDataFolder();
-    foreach ($folders as $folder) {
-        $checks[sprintf($ptx['syscheck_writable'], $folder)]
-            = is_writable($folder) ? 'ok' : 'warn';
-    }
-    $icon = $pth['folder']['plugins'] . 'boilerplate/boilerplate.png';
-    $version = BOILERPLATE_VERSION;
-    $bag = compact('labels', 'images', 'checks', 'icon', 'version');
-    return Boilerplate_render('info', $bag);
-}
-
-/**
- * Creates a new boilerplate file. Redirects to the edit view on success;
- * shows the main administration on failure.
- *
- * @param string $name A boilerplate name.
- *
- * @return string (X)HTML.
- *
- * @global string            Error message to emit in the (X)HTML.
- * @global array             The localization of the plugins.
- * @global Boilerplate\Model The model.
- */
-function Boilerplate_new($name)
-{
-    global $e, $plugin_tx, $_Boilerplate;
-
-    Boilerplate_checkCsrfToken();
-    $ptx = $plugin_tx['boilerplate'];
-    if (!$_Boilerplate->isValidName($name)) {
-        $e .= '<li><b>' . $ptx['error_invalid_name'] . '</b><br>'
-            . $name . '</li>' . PHP_EOL;
-        return Boilerplate_admin();
-    }
-    $fn = $_Boilerplate->filename($name);
-    if (!file_exists($fn)) {
-        if ($_Boilerplate->write($name, '') !== false) {
-            $qs = '?boilerplate&admin=plugin_main&action=edit&boilerplate_name='
-                . $name;
-            header('Location: ' . CMSIMPLE_URL . $qs, true, 303);
-            exit;
-        } else {
-            e('cntwriteto', 'file', $fn);
-            return Boilerplate_admin();
-        }
-    } else {
-        e('alreadyexists', 'file', $fn);
-        return Boilerplate_admin();
-    }
-}
-
-/**
- * Returns the boilerplate edit view.
- *
- * @param string $name    A boilerplate name.
- * @param string $content A boilerplate content.
- *
- * @return string (X)HTML.
- *
- * @global string            The script name.
- * @global array             The paths of system files and folders.
- * @global array             The configuration of the core.
- * @global array             The localization of the core.
- * @global Boilerplate\Model The model.
- */
-function Boilerplate_edit($name, $content = null)
-{
-    global $sn, $pth, $cf, $tx, $_Boilerplate;
-
-    if (!isset($content)) {
-        $content = $_Boilerplate->read($name);
-        if ($content === false) {
-            e('cntopen', 'file', $_Boilerplate->filename($name));
-            return false;
-        }
-    }
-    $labels = array(
-        'heading' => "Boilerplate \xE2\x80\x93 $name",
-        'save' => ucfirst($tx['action']['save'])
-    );
-    $labels = array_map('XH_hsc', $labels);
-    $url = "$sn?&amp;boilerplate";
-    $editorHeight = $cf['editor']['height'];
-    $content = XH_hsc($content);
-    $showSubmit = !function_exists('init_editor')
-        || $cf['editor']['external'] == '';
-    $bag = compact('labels', 'name', 'url', 'editorHeight', 'content', 'showSubmit');
-    $o = Boilerplate_render('edit', $bag);
-    init_editor(array('plugintextarea'));
-    return $o;
-}
-
-/**
- * Saves a boilerplate text. Redirects to the main administration on success;
- * returns the edit view on failure.
- *
- * @param string $name A boilerplate.
- *
- * @return string (X)HTML.
- *
- * @global Boilerplate\Model The model.
- */
-function Boilerplate_save($name)
-{
-    global $_Boilerplate;
-
-    Boilerplate_checkCsrfToken();
-    $content = $_POST['boilerplate_text'];
-    $ok = $_Boilerplate->write($name, $content);
-    if ($ok) {
-        $qs = '?boilerplate&admin=plugin_main&action=plugin_tx';
-        header('Location: ' . CMSIMPLE_URL . $qs, true, 303);
-        exit;
-    } else {
-        e('cntsave', 'file', $_Boilerplate->filename($name));
-        return Boilerplate_edit($name, $content);
-    }
-}
-
-/**
- * Deletes the boilerplate $name. Redirects to the main administration view on
- * success; shows the main administration on failure.
- *
- * @param string $name A boilerplate name.
- *
- * @return string (X)HTML.
- *
- * @global Boilerplate\Model The model.
- */
-function Boilerplate_delete($name)
-{
-    global $_Boilerplate;
-
-    Boilerplate_checkCsrfToken();
-    if ($_Boilerplate->delete($name)) {
-        $qs = '?boilerplate&admin=plugin_main&action=plugin_tx';
-        header('Location: ' . CMSIMPLE_URL . $qs, true, 303);
-        exit;
-    } else {
-        e('cntdelete', 'file', $_Boilerplate->filename($name));
-        return Boilerplate_admin();
-    }
-}
-
-/**
- * Returns the main administration view.
- *
- * @return string (X)HTML.
- *
- * @global string            The script name.
- * @global array             The paths of system files and folders.
- * @global array             The localization of the core.
- * @global array             The localization of the plugins.
- * @global Boilerplate\Model The model.
- */
-function Boilerplate_admin()
-{
-    global $sn, $pth, $tx, $plugin_tx, $_Boilerplate;
-
-    $ptx = $plugin_tx['boilerplate'];
-    $labels = array(
-        'heading' => "Boilerplate \xE2\x80\x93 $ptx[menu_main]",
-        'edit' => ucfirst($tx['action']['edit']),
-        'delete' => ucfirst($tx['action']['delete']),
-        'create' => $ptx['label_create'],
-        'confirm' => addcslashes($ptx['confirm_delete'], "\r\n\\\'")
-    );
-    $labels = array_map('XH_hsc', $labels);
-    $deleteImage = $pth['folder']['plugins'] . 'boilerplate/images/delete.png';
-    $url = $sn . '?&amp;boilerplate';
-    $baseURL = $sn . '?&amp;boilerplate&amp;admin=plugin_main&amp;action=';
-    $boilerplates = array();
-    foreach ($_Boilerplate->names() as $name) {
-        $boilerplates[$name] = array(
-            'editURL' => $baseURL . 'edit&amp;boilerplate_name=' . $name,
-            'deleteURL' => $baseURL . 'delete&amp;boilerplate_name=' . $name
-        );
-    }
-    $bag = compact('labels', 'deleteImage', 'url', 'boilerplates');
-    return Boilerplate_render('admin', $bag);
-}
-
 /*
  * Register the plugin menu items.
  */
@@ -312,24 +97,24 @@ if (XH_wantsPluginAdministration('boilerplate')) {
     $o .= print_plugin_admin('on');
     switch ($admin) {
         case '':
-            $o .= Boilerplate_info();
+            $o .= (new Boilerplate\AdminController)->renderInfo();
             break;
         case 'plugin_main':
             switch ($action) {
                 case 'new':
-                    $o .= Boilerplate_new($_POST['boilerplate_name']);
+                    $o .= (new Boilerplate\AdminController)->newTextBlock($_POST['boilerplate_name']);
                     break;
                 case 'edit':
-                    $o .= Boilerplate_edit($_GET['boilerplate_name']);
+                    $o .= (new Boilerplate\AdminController)->editTextBlock($_GET['boilerplate_name']);
                     break;
                 case 'save':
-                    $o .= Boilerplate_save($_POST['boilerplate_name']);
+                    $o .= (new Boilerplate\AdminController)->saveTextBlock($_POST['boilerplate_name']);
                     break;
                 case 'delete':
-                    $o .= Boilerplate_delete($_POST['boilerplate_name']);
+                    $o .= (new Boilerplate\AdminController)->deleteTextBlock($_POST['boilerplate_name']);
                     break;
                 default:
-                    $o .= Boilerplate_admin();
+                    $o .= (new Boilerplate\AdminController)->renderMainAdministration();
             }
             break;
         default:
---
+++ classes/AdminController.php
@@ -0,0 +1,240 @@
+<?php
+
+/**
+ * Copyright 2013-2021 Christoph M. Becker
+ *
+ * This file is part of Boilerplate_XH.
+ *
+ * Boilerplate_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Boilerplate_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Boilerplate_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Boilerplate;
+
+class AdminController
+{
+    /**
+     * Returns the plugin information view.
+     *
+     * @return string (X)HTML.
+     *
+     * @global array             The paths of system files and folders.
+     * @global array             The localization of the core.
+     * @global array             The localization of the plugins.
+     * @global Boilerplate\Model The model.
+     */
+    public function renderInfo()
+    {
+        global $pth, $tx, $plugin_tx, $_Boilerplate;
+
+        $ptx = $plugin_tx['boilerplate'];
+        $labels = array(
+            'info' => $ptx['label_info'],
+            'syscheck' => $ptx['syscheck_title'],
+            'about' => $ptx['about'],
+            'logo' => $ptx['alt_logo']
+        );
+        $labels = array_map('XH_hsc', $labels);
+        $phpVersion = '5.4.0';
+        foreach (array('ok', 'warn', 'fail') as $state) {
+            $images[$state] = $pth['folder']['plugins']
+                . "boilerplate/images/$state.png";
+        }
+        $checks = array();
+        $checks[sprintf($ptx['syscheck_phpversion'], $phpVersion)]
+            = version_compare(PHP_VERSION, $phpVersion) >= 0 ? 'ok' : 'fail';
+        foreach (array('config/', 'css', 'languages/') as $folder) {
+            $folders[] = $pth['folder']['plugins'] . 'boilerplate/' . $folder;
+        }
+        $folders[] = $_Boilerplate->getDataFolder();
+        foreach ($folders as $folder) {
+            $checks[sprintf($ptx['syscheck_writable'], $folder)]
+                = is_writable($folder) ? 'ok' : 'warn';
+        }
+        $icon = $pth['folder']['plugins'] . 'boilerplate/boilerplate.png';
+        $version = BOILERPLATE_VERSION;
+        $bag = compact('labels', 'images', 'checks', 'icon', 'version');
+        return Boilerplate_render('info', $bag);
+    }
+
+    /**
+     * Creates a new boilerplate file. Redirects to the edit view on success;
+     * shows the main administration on failure.
+     *
+     * @param string $name A boilerplate name.
+     *
+     * @return string (X)HTML.
+     *
+     * @global string            Error message to emit in the (X)HTML.
+     * @global array             The localization of the plugins.
+     * @global Boilerplate\Model The model.
+     */
+    public function newTextBlock($name)
+    {
+        global $e, $plugin_tx, $_Boilerplate;
+
+        Boilerplate_checkCsrfToken();
+        $ptx = $plugin_tx['boilerplate'];
+        if (!$_Boilerplate->isValidName($name)) {
+            $e .= '<li><b>' . $ptx['error_invalid_name'] . '</b><br>'
+                . $name . '</li>' . PHP_EOL;
+            return $this->renderMainAdministration();
+        }
+        $fn = $_Boilerplate->filename($name);
+        if (!file_exists($fn)) {
+            if ($_Boilerplate->write($name, '') !== false) {
+                $qs = '?boilerplate&admin=plugin_main&action=edit&boilerplate_name='
+                    . $name;
+                header('Location: ' . CMSIMPLE_URL . $qs, true, 303);
+                exit;
+            } else {
+                e('cntwriteto', 'file', $fn);
+                return $this->renderMainAdministration();
+            }
+        } else {
+            e('alreadyexists', 'file', $fn);
+            return $this->renderMainAdministration();
+        }
+    }
+
+    /**
+     * Returns the boilerplate edit view.
+     *
+     * @param string $name    A boilerplate name.
+     * @param string $content A boilerplate content.
+     *
+     * @return string (X)HTML.
+     *
+     * @global string            The script name.
+     * @global array             The paths of system files and folders.
+     * @global array             The configuration of the core.
+     * @global array             The localization of the core.
+     * @global Boilerplate\Model The model.
+     */
+    public function editTextBlock($name, $content = null)
+    {
+        global $sn, $pth, $cf, $tx, $_Boilerplate;
+
+        if (!isset($content)) {
+            $content = $_Boilerplate->read($name);
+            if ($content === false) {
+                e('cntopen', 'file', $_Boilerplate->filename($name));
+                return false;
+            }
+        }
+        $labels = array(
+            'heading' => "Boilerplate \xE2\x80\x93 $name",
+            'save' => ucfirst($tx['action']['save'])
+        );
+        $labels = array_map('XH_hsc', $labels);
+        $url = "$sn?&amp;boilerplate";
+        $editorHeight = $cf['editor']['height'];
+        $content = XH_hsc($content);
+        $showSubmit = !function_exists('init_editor')
+            || $cf['editor']['external'] == '';
+        $bag = compact('labels', 'name', 'url', 'editorHeight', 'content', 'showSubmit');
+        $o = Boilerplate_render('edit', $bag);
+        init_editor(array('plugintextarea'));
+        return $o;
+    }
+
+    /**
+     * Saves a boilerplate text. Redirects to the main administration on success;
+     * returns the edit view on failure.
+     *
+     * @param string $name A boilerplate.
+     *
+     * @return string (X)HTML.
+     *
+     * @global Boilerplate\Model The model.
+     */
+    public function saveTextBlock($name)
+    {
+        global $_Boilerplate;
+
+        Boilerplate_checkCsrfToken();
+        $content = $_POST['boilerplate_text'];
+        $ok = $_Boilerplate->write($name, $content);
+        if ($ok) {
+            $qs = '?boilerplate&admin=plugin_main&action=plugin_tx';
+            header('Location: ' . CMSIMPLE_URL . $qs, true, 303);
+            exit;
+        } else {
+            e('cntsave', 'file', $_Boilerplate->filename($name));
+            return $this->editTextBlock($name, $content);
+        }
+    }
+
+    /**
+     * Deletes the boilerplate $name. Redirects to the main administration view on
+     * success; shows the main administration on failure.
+     *
+     * @param string $name A boilerplate name.
+     *
+     * @return string (X)HTML.
+     *
+     * @global Boilerplate\Model The model.
+     */
+    public function deleteTextBlock($name)
+    {
+        global $_Boilerplate;
+
+        Boilerplate_checkCsrfToken();
+        if ($_Boilerplate->delete($name)) {
+            $qs = '?boilerplate&admin=plugin_main&action=plugin_tx';
+            header('Location: ' . CMSIMPLE_URL . $qs, true, 303);
+            exit;
+        } else {
+            e('cntdelete', 'file', $_Boilerplate->filename($name));
+            return $this->renderMainAdministration();
+        }
+    }
+
+    /**
+     * Returns the main administration view.
+     *
+     * @return string (X)HTML.
+     *
+     * @global string            The script name.
+     * @global array             The paths of system files and folders.
+     * @global array             The localization of the core.
+     * @global array             The localization of the plugins.
+     * @global Boilerplate\Model The model.
+     */
+    public function renderMainAdministration()
+    {
+        global $sn, $pth, $tx, $plugin_tx, $_Boilerplate;
+
+        $ptx = $plugin_tx['boilerplate'];
+        $labels = array(
+            'heading' => "Boilerplate \xE2\x80\x93 $ptx[menu_main]",
+            'edit' => ucfirst($tx['action']['edit']),
+            'delete' => ucfirst($tx['action']['delete']),
+            'create' => $ptx['label_create'],
+            'confirm' => addcslashes($ptx['confirm_delete'], "\r\n\\\'")
+        );
+        $labels = array_map('XH_hsc', $labels);
+        $deleteImage = $pth['folder']['plugins'] . 'boilerplate/images/delete.png';
+        $url = $sn . '?&amp;boilerplate';
+        $baseURL = $sn . '?&amp;boilerplate&amp;admin=plugin_main&amp;action=';
+        $boilerplates = array();
+        foreach ($_Boilerplate->names() as $name) {
+            $boilerplates[$name] = array(
+                'editURL' => $baseURL . 'edit&amp;boilerplate_name=' . $name,
+                'deleteURL' => $baseURL . 'delete&amp;boilerplate_name=' . $name
+            );
+        }
+        $bag = compact('labels', 'deleteImage', 'url', 'boilerplates');
+        return Boilerplate_render('admin', $bag);
+    }
+}
