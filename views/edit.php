f1dc9121dff612ada8507e534d28c0d22503b490
Christoph M. Becker <cmbecker69@gmx.de>
2021-01-10 00:58:18 +0100

    Properly indent templates

--- views/edit.php
+++ views/edit.php
@@ -6,11 +6,12 @@ if (!isset($this)) {
 ?>
 <!-- Boilerplate_XH: edit -->
 <h1>Boilerplate – <?=$name?></h1>
-    <form class="plugineditform" action="<?=$url?>" method="post">
-        <textarea class="plugintextarea" name="boilerplate_text" style="height: <?=$editorHeight?>px"><?=$content?></textarea>
-        <input type="hidden" name="admin" value="plugin_main"/>
-        <input type="hidden" name="action" value="save"/>
-        <input type="hidden" name="boilerplate_name" value="<?=$name?>"/>
-        <?=$this->csrfToken()?>
-    </form>
-</div>
+<form class="plugineditform" action="<?=$url?>" method="post">
+    <textarea class="plugintextarea" name="boilerplate_text" style="height: <?=$editorHeight?>px">
+        <?=$content?>
+    </textarea>
+    <input type="hidden" name="admin" value="plugin_main"/>
+    <input type="hidden" name="action" value="save"/>
+    <input type="hidden" name="boilerplate_name" value="<?=$name?>"/>
+    <?=$this->csrfToken()?>
+</form>
--- views/info.php
+++ views/info.php
@@ -19,17 +19,23 @@ if (!isset($this)) {
 <hr/>
 <h4><?=$this->text('about')?></h4>
 <img src="<?=$icon?>" style="float: left; width: 128px; height: 128px; margin-right: 16px"
-alt="<?=$this->text('alt_logo')?>"/>
+    alt="<?=$this->text('alt_logo')?>"/>
 <p>Version: <?=$version?></p>
 <p>Copyright &copy; 2012-2021 <a href="http://3-magi.net/">Christoph M. Becker</a></p>
-<p style="text-align: justify">This program is free software: you can
-redistribute it and/or modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.</p>
-<p style="text-align: justify">This program is distributed in the hope that it
-will be useful, but <em>without any warranty</em>; without even the implied warranty of
-<em>merchantability</em> or <em>fitness for a particular purpose</em>. See the GNU General
-Public License for more details.</p>
-<p style="text-align: justify">You should have received a copy of the GNU
-General Public License along with this program. If not, see
-<a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
+<p style="text-align: justify">
+    This program is free software: you can
+    redistribute it and/or modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+</p>
+<p style="text-align: justify">
+    This program is distributed in the hope that it
+    will be useful, but <em>without any warranty</em>; without even the implied warranty of
+    <em>merchantability</em> or <em>fitness for a particular purpose</em>. See the GNU General
+    Public License for more details.
+</p>
+<p style="text-align: justify">
+    You should have received a copy of the GNU
+    General Public License along with this program. If not, see
+    <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+</p>
