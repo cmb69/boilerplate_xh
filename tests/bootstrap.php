2cdbfb81d1b4c9e265479ad8e6f4b4a858144c23
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-19 13:24:10 +0100

    The version constants are still required

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,3 +17,6 @@ require_once './classes/AdminController.php';
 require_once './classes/BoilerplateController.php';
 require_once './classes/Dic.php';
 require_once './classes/InfoController.php';
+
+const CMSIMPLE_XH_VERSION = "1.8";
+const BOILERPLATE_VERSION = "2.1-dev";
