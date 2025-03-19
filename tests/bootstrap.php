576dbff9805030af123b7d2adad24b89355b80ac
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-19 13:05:16 +0100

    Drop now superfluous const declarations in bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,3 @@ require_once './classes/AdminController.php';
 require_once './classes/BoilerplateController.php';
 require_once './classes/Dic.php';
 require_once './classes/InfoController.php';
-
-const CMSIMPLE_XH_VERSION = "1.8";
-const CMSIMPLE_URL = "http://example.com/";
-const BOILERPLATE_VERSION = "2.1-dev";
